b25b2b1147fa8adb84baa6d1527452d27259eaeb
Spencer Willis <142636930+SpennyWill@users.noreply.github.com>
2023-10-27 16:03:42 -0400

    Schnazzy Kelvin project
    added some interpolation to show all the temperatures

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+
 // 293 kelvin is our base temp, this will be a reference for the other temps in F and C
-const kelvin = 273; 
+const kelvin = 293; 
+console.log(`The temperature is ${kelvin} Kelvin`)
 // Celsius is 273 degrees less than kelvin
 const celsius = kelvin - 273;
+console.log(` The temperature in Celsius is ${celsius} degrees`)
 // the conversion of fahrenheit to celsius is the annoying one below. Not sure why Americans make things so difficult for themselves. We are using let becuase we will be rounding the number in the next step
 let fahrenheit = celsius * (9/5) + 32; 
 // this is where we make a whole number for fahrenheit
 fahrenheit = Math.floor(fahrenheit); 
-console.log(`The temperature is ${fahrenheit} degrees fahrenheit`);
+console.log(`The temperature in fahrenheit is ${fahrenheit} degrees `);
 
 let Newton = celsius * (33/100); 
 Newton = Math.floor(Newton); 
